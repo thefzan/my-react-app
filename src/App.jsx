53a91583a538f1b8ff60bdc9b1df4235ad0aadbb
Syed Faizan Ahmad <149227775+thefzan@users.noreply.github.com>
2025-11-03 15:38:39 +0530

    Fix: cache issue and redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
-import Slider from "./components/Slider";
 import Body from "./Body";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Footer from "./components/Footer";
-function App() {
-  const [count, setCount] = useState(0);
+import { Routes, Route } from "react-router-dom";
 
+function App() {
   return (
-    <Router>
+    <>
       <Nav />
-      
       <Routes>
-        <Route path="/" element={<Body />}/>
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Body />} />
+        <Route path="about" element={<About />} />
       </Routes>
-      <Footer/>
-    </Router>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
